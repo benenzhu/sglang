1c564774631b3482d93384c8225240ac9023b99a
zhutaoyu <zhutaoyu97@gmail.com>
2025-11-27 08:33:51 +0000

    patch sglang

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,6 @@
 set -e
 set -x 
+cp sgl-kernel/python/sgl_kernel/hadamard.py /opt/venv/lib/python3.10/site-packages/sgl_kernel-0.3.18-py3.10-linux-x86_64.egg/sgl_kernel/hadamard.py
 export MODEL="/data/DeepSeek-R1-0528"
 export MODEL="/data/DeepSeek-V3.2-Exp"
 export PORT=30000
